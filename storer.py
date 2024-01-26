586f3bde1ba37be0c7de79f109e8e3ae04d095a4
Daniel Ha <f003f9h@dartmouth.edu>
2024-01-26 11:32:51 -0800

    adding comments

--- storer.py
+++ storer.py
@@ -2,6 +2,7 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
+#main function to store everything!
 def store_stuff(notif_info):
     user_id, first_name = notif_info[0][0], notif_info[0][1]
 
@@ -18,7 +19,43 @@ def store_stuff(notif_info):
         finally:
             cnx.close()
 
-#function to 
+####### STORING FUNCTIONS ###########
+
+#function to store a new user to the database
+def add_user(cnx, user_id, user_first_name):
+    try:
+        cursor = cnx.cursor()
+        add_user_query = """
+        INSERT INTO users (user_id, first_name)
+        VALUES (%s, %s);
+        """
+        cursor.execute(add_user_query, (user_id, user_first_name))
+        cnx.commit()
+        print(f"User added with user_id: {user_id}")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+
+#function to store conditions
+def store_condition_and_return_id(cnx, column_name, comparator, threshold):
+    try:
+        cursor = cnx.cursor()
+        add_condition_query="""
+        INSERT INTO conditions (column_name, comparator, threshold)
+        values (%s, %s, %s);
+        """
+        cursor.execute(add_condition_query, (column_name, comparator, threshold))
+        print(f"condition added")
+    except mysql.connector.Error as err:
+        print(f"Error:{err}")
+    finally:
+        cursor.close()
+
+#######    HELPER FUNCTIONS      ###########
+
+#function to check if the user already exists in the database!
 def does_user_already_exist(cnx, user_id):
     try:
         cursor = cnx.cursor()
@@ -34,6 +71,7 @@ def does_user_already_exist(cnx, user_id):
             print(f"Error when checking if user already exists: {err}")
             return False
 
+#function to connect to the database, returns a connection instance
 def connect_to_db():
     load_dotenv()
     DB_USER = os.getenv('DB_USER')
@@ -56,33 +94,3 @@ def connect_to_db():
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None
-
-def add_user(cnx, user_id, user_first_name):
-    try:
-        cursor = cnx.cursor()
-        add_user_query = """
-        INSERT INTO users (user_id, first_name)
-        VALUES (%s, %s);
-        """
-        cursor.execute(add_user_query, (user_id, user_first_name))
-        cnx.commit()
-        print(f"User added with user_id: {user_id}")
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    finally:
-        cursor.close()
-
-def store_condition_and_return_id(cnx, column_name, comparator, threshold):
-    try:
-        cursor = cnx.cursor()
-        add_condition_query="""
-        INSERT INTO conditions (column_name, comparator, threshold)
-        values (%s, %s, %s);
-        """
-        cursor.execute(add_condition_query, (column_name, comparator, threshold))
-        print(f"condition added")
-    except mysql.connector.Error as err:
-        print(f"Error:{err}")
-    finally:
-        cursor.close()
