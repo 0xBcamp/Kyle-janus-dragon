286a7f4ad225acbd2f2dcecfcd94391f5259a012
adam-gill <110919227+adam-gill@users.noreply.github.com>
2024-01-26 13:03:35 -0500

    test

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import telebot
 from dotenv import load_dotenv
 from storer import store_stuff
 # Load environment variables
+#test
 load_dotenv()
 TG_API_KEY = os.getenv('TG_API_KEY')
 CMC_API_KEY = os.getenv('CMC_API_KEY')
