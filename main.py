a4a1d559944aad02d53f937b597008d535d0c65a
Daniel Ha <f003f9h@dartmouth.edu>
2024-01-27 13:50:53 -0800

    adding comments at the top of main file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 """
 main.py
-Usage: call with "python3 main.py"
+Usage: call with "./main.py". If permissino denied, run this command first "chmod +x main.py".
 """
 
 from bot.bot import create_bot
