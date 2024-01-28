839a6f49decd3903abae0fe248b0577002e991ae
Daniel Ha <f003f9h@dartmouth.edu>
2024-01-27 16:26:14 -0800

    adding extra instructions

--- main.py
+++ main.py
@@ -2,7 +2,19 @@
 
 """
 main.py
-Usage: call with "./main.py". If permissino denied, run this command first "chmod +x main.py".
+Usage: 
+1. First, activate virtual environment with command:
+    "source .venv/bin/activate "
+2. Create a .env file and fill in this info:
+    TG_API_KEY=
+    CMC_API_KEY=
+    DB_USER=
+    DB_PASSWORD=
+    DB_HOST=
+    DEBUG=
+2. Then, start the telegram bot with "./main.py". 
+2a. If permission denied, run this command first "chmod +x main.py".
+3. Message your Telegram bot /help to see how to interact with it!
 """
 
 from bot.bot import create_bot
