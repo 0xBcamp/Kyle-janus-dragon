be11ecbf613acc0cd9194bc223c3b8ec9ece9d8c
Daniel Ha <f003f9h@dartmouth.edu>
2024-02-01 21:38:46 -0800

    changing to typescript

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '@/styles/styles.css'
-import '@/styles/mediaqueries.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+// Assuming this is a standard page component (e.g., pages/home.tsx)
+import type { NextPage } from 'next';
+import Home_Page from './components/Home_page';
+import Navbar from './components/Navbar';
+
+const Home: NextPage = () => {
+  // Your page content here
+  return (
+    <div>
+      <Navbar/>
+      <Home_Page/>
+    </div>
+  );
+};
+
+export default Home;
