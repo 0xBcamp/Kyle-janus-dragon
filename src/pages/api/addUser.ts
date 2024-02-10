bd9da46cd6b451b86ec5259c87950a0ce5a5de0b
Daniel Ha <f003f9h@dartmouth.edu>
2024-02-09 20:45:48 -0800

    added a very flexible adduser interface

--- src/pages/api/addUser.ts
+++ src/pages/api/addUser.ts
@@ -1,6 +1,7 @@
 // pages/api/addUser.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {mysql, RowDataPacket, FieldPacket} from 'mysql2/promise';
+import mysql from 'mysql2/promise';
+import { RowDataPacket, FieldPacket } from 'mysql2';
 
 export default async function addUser(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
@@ -19,12 +20,13 @@ export default async function addUser(req: NextApiRequest, res: NextApiResponse)
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
         });
-
+        
         // First, check if the user already exists
         const checkSql = 'SELECT email FROM moon_users WHERE email = ? LIMIT 1';
         const [users] = await connection.query(checkSql, [email]);
         // if the user doesn't yet exist...
         if (users.length == 0) {
+            console.log('new user of this UI:', email, '...');
             //...add the user!
             const addUserSQL = 'INSERT INTO moon_users (email, unnamed_num) VALUES (?, ?)';
             let unnamed_num = 1;
@@ -36,18 +38,22 @@ export default async function addUser(req: NextApiRequest, res: NextApiResponse)
                     // Correctly interpolate unnamed_num in the address name
                     await connection.execute(insertAddressSql, [email, address, `Unnamed Address ${unnamed_num}`]);
                     unnamed_num += 1;
+                    console.log('existing address added', address);
+                    console.log('unnamed_num increased to', unnamed_num);
                 }
                 // After inserting addresses, update the user's unnamed_num value with the final count
                 const updateUnnamedNumSQL = 'UPDATE moon_users SET unnamed_num = ? WHERE email = ?';
                 // Note the reversed order of parameters compared to the INSERT query
                 await connection.query(updateUnnamedNumSQL, [unnamed_num, email]);
+
             }
+            console.log('new user unnamed num:', unnamed_num);
             //...then, update the user's unnamed_num value!
             return res.status(200).json({ message: 'new User added!' });
         }
         //if the user does exist already...
         else{
-            console.log('repeat user');
+            console.log('welcoming back', users[0].email, '...');
             //check if the user has changed their addresses when not using our interface
             //if the user deleted all of their addresses when gone, remove from our database
             if (addresses.length == 0) {
@@ -64,37 +70,50 @@ export default async function addUser(req: NextApiRequest, res: NextApiResponse)
                     acc[address] = false; // Initialize all as not found (false)
                     return acc;
                 }, {});
+                console.log('got previously stored user addresses (automatically set to false):', previousUserAddressesDict);
+                console.log('will set an address to true if the address has not been deleted...');
 
                 // Get the current unnamed_num for the user
                 const getUnnamedNum = 'SELECT unnamed_num FROM moon_users WHERE email = ?';
                 const [[{ unnamed_num: unnamedNumResult }]] = await connection.query(getUnnamedNum, [email]) as [RowDataPacket[], FieldPacket[]]; // Type assertion here
                 let unnamedNum = unnamedNumResult;
+                let newUnnamedNum = unnamedNum;
 
                 for (const address of addresses){
                     //1. check if the address is still in the previous user addresses:
                     //if the address is not in the previous...
-                    if (!(address in previousUserAddresses)){
+                    if (!(address in previousUserAddressesDict)){
                         //add it!
+                        console.log('previous addr')
                         const addNewOutsideUserAddress = 'INSERT INTO moon_addresses (email, moon_address, address_name) VALUES (?, ?, ?)';
                         await connection.execute(addNewOutsideUserAddress, [email. unnamedNum]);
-                        unnamedNum += 1;
+                        newUnnamedNum += 1;
+                        console.log('address not in previous!:', address);
                     }
                     //if the address is in the previous addresses, set its dictionary value to true
                     else{
-                        previousUserAddresses[address] = true;
+                        previousUserAddressesDict[address] = true;
+                        console.log(address, 'set to true');
                     }
                 }
-                // At the end of all of this, update your unnamed_num...
-                const updateUnnamedNumSQL = 'UPDATE moon_users SET unnamed_num = ? WHERE email = ?';
-                await connection.query(updateUnnamedNumSQL, [unnamedNum, email]);
+                // At the end of all of this, update your unnamed_num if necessary:
+                console.log('unnamedNum: ', unnamedNum);
+                if (newUnnamedNum > unnamedNum){
+                    const updateUnnamedNumSQL = 'UPDATE moon_users SET unnamed_num = ? WHERE email = ?';
+                    await connection.query(updateUnnamedNumSQL, [newUnnamedNum, email]);
+                    console.log('updated unnamed_num to', newUnnamedNum);
+                }
                 //...and loop through the dictionary, deleting all addresses from the database that are still set to false
+                console.log('deleting addresses deleted outside of this UI...');
                 for (const address in previousUserAddressesDict){
                     if (previousUserAddressesDict[address] == false ){
                         const deleteUserAddress = 'DELETE FROM moon_addresses WHERE email = ? AND moon_address = ?';
                         await connection.execute(deleteUserAddress, [email]);
+                        console.log('deleted', address)
                     }
                 }
             }
+            res.status(200).json({ message: 'welcomed back user!' });
         }
 
         // Start a transaction
